efe5c367c4be2bd05d2d63d598b78de1b0be113d
Daniel Scholz <danielsch@gmx.net>
2022-05-01 11:48:36 +0200

    Updated Libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,13 @@ description = "FileIndexerAndBackup"
 java.sourceCompatibility = JavaVersion.VERSION_11
 java.targetCompatibility = JavaVersion.VERSION_11
 
-val kotlinVersion = "1.6.10"
-val coroutinesVersion = "1.6.0"
+val kotlinVersion = "1.6.21"
+val coroutinesVersion = "1.6.1"
 
 plugins {
    java
    application
-   kotlin("jvm") version "1.6.10"
+   kotlin("jvm") version "1.6.21"
    id("org.jetbrains.compose") version "1.0.1"
 }
 
@@ -35,12 +35,12 @@ dependencies {
    implementation("com.drewnoakes:metadata-extractor:2.16.0")
    implementation("org.apache.commons:commons-lang3:3.12.0")
    implementation("org.apache.commons:commons-compress:1.21")
-   implementation("org.slf4j:slf4j-api:1.7.32")
-   implementation("ch.qos.logback:logback-core:1.2.10")
-   implementation("ch.qos.logback:logback-classic:1.2.10")
+   implementation("org.slf4j:slf4j-api:1.7.36")
+   implementation("ch.qos.logback:logback-core:1.2.11")
+   implementation("ch.qos.logback:logback-classic:1.2.11")
    implementation("org.xerial:sqlite-jdbc:3.36.0.2")
    implementation("org.tukaani:xz:1.9")
-   implementation("com.google.guava:guava:31.0.1-jre") {
+   implementation("com.google.guava:guava:31.1-jre") {
       exclude(group = "com.google.code.findbugs", module = "jsr305")
       exclude(group = "com.google.errorprone", module = "error_prone_annotations")
       exclude(group = "com.google.j2objc", module = "j2objc-annotations")
